6821f993f35b1cba329785d7dab45b9ae7c38240
Khuselo Nditha <khuselonditha@gmail.com>
2025-01-21 15:55:00 +0200

    After creating our DAG.

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -6,6 +6,17 @@ from airflow.operators.python import PythonOperator         # Used to fecth our 
 # Setting default args for our DAG
 default_args = {
     "owner": 'khuselo',
-    "start_date": datetime(2025, 1, 21, 16, 00) 
+    "start_date": datetime(2025, 1, 21, 17, 00) 
 }
+
+# Create DAG
+with DAG("user_automation",
+        default_args=default_args,
+        schedule_interval='@daily',
+        catchup=False) as dag:
+    
+    streaming_task = PythonOperator(
+        task_id= 'stream_data_from_api',
+        python_callable=stream_data
+    )
 
