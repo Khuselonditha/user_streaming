7363121f3e85a601b487a620b9448da5787c87e6
Khuselo Nditha <khuselonditha@gmail.com>
2025-01-21 16:16:57 +0200

    After creating a function to get data from API.

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -2,6 +2,8 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator         # Used to fecth our data
+import json
+import requests
 
 # Setting default args for our DAG
 default_args = {
@@ -9,14 +11,26 @@ default_args = {
     "start_date": datetime(2025, 1, 21, 17, 00) 
 }
 
-# Create DAG
-with DAG("user_automation",
-        default_args=default_args,
-        schedule_interval='@daily',
-        catchup=False) as dag:
-    
-    streaming_task = PythonOperator(
-        task_id= 'stream_data_from_api',
-        python_callable=stream_data
-    )
+# Get data function
+def get_data():
+    res = requests.get('https://randomuser.me/api/')
+    res = res.json()['results'][0]
 
+    return res
+
+# # Streaming function
+# def stream_data():
+#     print(json.dumps(res, indent=3))
+
+# Create DAG
+# with DAG("user_automation",
+#         default_args=default_args,
+#         schedule_interval='@daily',
+#         catchup=False) as dag:
+    
+#     streaming_task = PythonOperator(
+#         task_id= 'stream_data_from_api',
+#         python_callable=stream_data()
+#     )
+
+stream_data()
