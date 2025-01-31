05109e48ab883fe656111cffa358dcf2fc7ec2ee
Khuselo Nditha <khuselonditha@gmail.com>
2025-01-31 21:02:05 +0200

    After adding function skeletons

--- spark_stream.py
+++ spark_stream.py
@@ -5,3 +5,19 @@ from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 
+def create_keyspace(session):
+    """Create keyspace here"""
+
+def create_table(session):
+    """Create a table here"""
+
+def insert_data(session):
+    """Insert data here"""
+
+def create_spark_connection():
+    """Create spark connection"""
+
+def create_cassandra_connection():
+    """Create a cassandra connection"""
+
+
