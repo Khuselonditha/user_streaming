55943d7105f3c581971bc3d775ad324a490d2537
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-04 16:23:02 +0200

    After creating a cassandra connection.

--- spark_stream.py
+++ spark_stream.py
@@ -35,7 +35,16 @@ def create_spark_connection():
 
 def create_cassandra_connection():
     """Create a cassandra connection"""
+    session = None
 
+    try:
+        cluster = Cluster(["localhost"])
+
+        session = cluster.connect()
+    except Exception as e:
+        logging.error(f"Couldn't create the spark connection due to {e}")
+
+    return session
 
 if __name__ == "__main__":
     spark_conn = create_spark_connection()
